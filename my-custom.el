7d592d8250671a997e408b3cbe050cf984560aa1
蒼蜂 <goalwill@live.jp>
2025-08-18 14:42:07 +0900

    Update my-custom.el

--- my-custom.el
+++ my-custom.el
@@ -540,6 +540,19 @@
 
 ;;(provide 'setup-org)
 
+;;; avy 希望するところにジャンプする
+;;; 
+(use-package avy
+  :ensure t
+  :config
+  (global-set-key (kbd "C-:") 'avy-goto-char)
+  (global-set-key (kbd "C-'") 'avy-goto-char-2)
+  (global-set-key (kbd "M-g f") 'avy-goto-line)
+  (global-set-key (kbd "M-g w") 'avy-goto-word-1)
+  (global-set-key (kbd "M-g e") 'avy-goto-word-0)
+  (avy-setup-default)
+  (global-set-key (kbd "C-c C-j") 'avy-resume))
+
 
 
 ;;; end of file
