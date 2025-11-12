d9ecd8165d874a94960d9d59fa2931136a7125f7
蒼蜂 <goalwill@live.jp>
2025-11-12 11:10:13 +0900

    Clean up my-custom-short.el by removing comments
    Removed unused configurations and comments from my-custom-short.el.

--- my-custom-short.el
+++ my-custom-short.el
@@ -15,40 +15,6 @@
 ;; (require 'use-package)
 ;; (setq use-package-always-ensure t)
 
-;; -*- coding: utf-8 -*-
-
-;;; my-custom prelude
-;;;;;
-;;;;;  https://qiita.com/nobuyuki86/items/b089d953938df1a10a12#%E8%83%8C%E6%99%AF
-  (use-package emacs
-    :custom (completion-ignore-case t) ;; 小文字と大文字を区別しない
-    :hook ((java-mode . (lambda ()
-                          (setq-local indent-tabs-mode t))))
-    :config
-    (which-function-mode +1) ;; 現在の機能（関数名等）をモードライン上に表示します
-
-    ;; 日本語の文字コード設定
-    (set-language-environment "Japanese")
-    (prefer-coding-system 'utf-8)
-    (when (eq system-type 'windows-nt)
-      (set-file-name-coding-system 'cp932)
-      (set-keyboard-coding-system 'cp932)
-      (set-terminal-coding-system 'cp932))
-
-    (set-charset-priority 'ascii
-                          'japanese-jisx0208
-                          'latin-jisx0201
-                          'katakana-jisx0201
-                          'iso-8859-1
-                          'cp1252
-                          'unicode)
-    (set-coding-system-priority 'utf-8
-                                'euc-jp
-                                'iso-2022-jp
-                                'cp932))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 ;; ====================
 ;; Ivy（counsel なしで補完強化）
 ;; ====================
@@ -199,88 +165,13 @@
   (setq avy-dispatch-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l ?q ?w ?e ?r ?t ?y ?u ?i ?o ?p))
   )
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; python org-babel
-
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((python . t)
-   (shell . t)))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;; my custom elisp modules ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ddskk  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ddskk
-(use-package ddskk-autoloads
-  :ensure ddskk)
-(require 'skk)
-
-(global-set-key (kbd "\C-xj") 'skk-mode)
-;; (global-set-key "\C-xj" 'skk-auto-fill-mode) ;; 改行を自動入力する場合
-;; (global-set-key "\C-xt" 'skk-tutorial)       ;; チュートリアル
-(setq default-input-method "japanese-skk")
-
-;;skk-azik
-(setq skk-use-azik t)
-;; M + x skk-get
-
-
-;;;;;;;;;;;;;;;;;;;;; yatex ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; https://zenn.dev/maswag/books/latex-on-emacs/viewer/yatex
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  (use-package yatex
-    ;; YaTeX がインストールされていない場合、package.elを使ってインストールする
-    :ensure t
-    ;; :commands autoload するコマンドを指定
-    :commands (yatex-mode)
-    ;; :mode auto-mode-alist の設定
-    :mode (("\\.tex$" . yatex-mode)
-           ("\\.ltx$" . yatex-mode)
-           ("\\.cls$" . yatex-mode)
-           ("\\.sty$" . yatex-mode)
-           ("\\.clo$" . yatex-mode)
-           ("\\.bbl$" . yatex-mode))
-    :init
-    (setq YaTeX-inhibit-prefix-letter t)
-    ;; :config キーワードはライブラリをロードした後の設定などを記述します。
-    :config
-    (setq YaTeX-kanji-code nil)
-    (setq YaTeX-latex-message-code 'utf-8)
-    (setq YaTeX-use-LaTeX2e t)
-    (setq YaTeX-use-AMS-LaTeX t)
-;;    (setq tex-command "/Library/TeX/texbin/latexmk -pdf -pvc -view=none")
-;;    (setq tex-pdfview-command "/usr/bin/open -a Skim")
-    (auto-fill-mode 0)
-    ;; company-tabnineによる補完。companyについては後述
-       (set (make-local-variable 'company-backends) '(company-tabnine))
-    )
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; tomorrow-night-deepblue-theme.el (Emacs theme)
-;;; https://github.com/jamescherti/tomorrow-night-deepblue-theme.el?tab=readme-ov-file#tomorrow-night-deepblue-themeel-emacs-theme
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Installation
-;; To install tomorrow-night-deepblue-theme from MELPA:
-;; If you haven't already done so, add MELPA repository to your Emacs configuration.
-;; Add the following code to your Emacs init file to install tomorrow-night-deepblue from MELPA:
-
-(use-package tomorrow-night-deepblue-theme
-  :ensure t
-  :config
-  ;; Disable all themes and load the Tomorrow Night Deep Blue theme
-  (mapc #'disable-theme custom-enabled-themes)
-  ;; Load the tomorrow-night-deepblue theme
-  (load-theme 'tomorrow-night-deepblue t))
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; https://github.com/bbatsov/emacs.d/blob/master/init.el
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package ag)
+;; (use-package
+;;   :ensure t
+;;   ag)
 
 ;; (use-package projectile
 ;;   :init
@@ -303,25 +194,23 @@
 
 
 
-(use-package elisp-slime-nav
-  :config
-  (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
-    (add-hook hook #'elisp-slime-nav-mode))
-  (diminish 'elisp-slime-nav-mode))
+;; (use-package elisp-slime-nav
+;;   :config
+;;   (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
+;;     (add-hook hook #'elisp-slime-nav-mode))
+;;   (diminish 'elisp-slime-nav-mode))
 
- (use-package paredit
-   :config
-   (define-key paredit-mode-map (kbd "RET") nil)
-   (add-hook 'emacs-lisp-mode-hook #'paredit-mode)
-   ;; enable in the *scratch* buffer
-   (add-hook 'lisp-interaction-mode-hook #'paredit-mode)
-   (add-hook 'ielm-mode-hook #'paredit-mode)
-   (add-hook 'lisp-mode-hook #'paredit-mode)
-   (add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode)
-   (diminish 'paredit-mode "()"))
+(use-package paredit
+  :ensure t
+  :hook ((emacs-lisp-mode . paredit-mode)
+         (lisp-mode . paredit-mode)          ;; Common Lisp, Schemeなどlisp系全般
+         (scheme-mode . paredit-mode)        ;; Scheme専用メジャーモード
+         (c-mode . paredit-mode)))            ;; C言語でも使いたい場合（普通はあまり使わないが要望に応じて）
+
 
 
 (use-package easy-kill
+  :ensure t
   :config
   (global-set-key [remap kill-ring-save] 'easy-kill))
 
@@ -344,45 +233,45 @@
 ;;               ("C-n" . 'copilot-next-completion)
 ;;               ("C-p" . 'copilot-previous-completion)))
 
-(use-package elisp-mode
-  :ensure nil ; not a real package
-  :config
-  (defun bozhidar-visit-ielm ()
-    "Switch to default `ielm' buffer.
-Start `ielm' if it's not already running."
-    (interactive)
-    (require 'crux)
-    (crux-start-or-switch-to 'ielm "*ielm*"))
+;; (use-package elisp-mode
+;;   :ensure nil ; not a real package
+;;   :config
+;;   (defun bozhidar-visit-ielm ()
+;;     "Switch to default `ielm' buffer.
+;; Start `ielm' if it's not already running."
+;;     (interactive)
+;;     (require 'crux)
+;;     (crux-start-or-switch-to 'ielm "*ielm*"))
 
-  (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
-  (define-key emacs-lisp-mode-map (kbd "C-c C-z") #'bozhidar-visit-ielm)
-  (define-key emacs-lisp-mode-map (kbd "C-c C-c") #'eval-defun)
-  (define-key emacs-lisp-mode-map (kbd "C-c C-b") #'eval-buffer))
+;;   (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
+;;   (define-key emacs-lisp-mode-map (kbd "C-c C-z") #'bozhidar-visit-ielm)
+;;   (define-key emacs-lisp-mode-map (kbd "C-c C-c") #'eval-defun)
+;;   (define-key emacs-lisp-mode-map (kbd "C-c C-b") #'eval-buffer))
 
-;; Windows-specific setup
-(when (eq system-type 'windows-nt)
-  (setq w32-pass-lwindow-to-system nil)
-  (setq w32-lwindow-modifier 'super) ; Left Windows key
+;; ;; Windows-specific setup
+;; (when (eq system-type 'windows-nt)
+;;   (setq w32-pass-lwindow-to-system nil)
+;;   (setq w32-lwindow-modifier 'super) ; Left Windows key
 
-  (setq w32-pass-rwindow-to-system nil)
-  (setq w32-rwindow-modifier 'super) ; Right Windows key
+;;   (setq w32-pass-rwindow-to-system nil)
+;;   (setq w32-rwindow-modifier 'super) ; Right Windows key
 
-  (setq w32-pass-apps-to-system nil)
-  (setq w32-apps-modifier 'hyper) ; Menu/App key
+;;   (setq w32-pass-apps-to-system nil)
+;;   (setq w32-apps-modifier 'hyper) ; Menu/App key
 
-  (set-frame-font "Cascadia Code 14")
-  (add-to-list 'exec-path "C:/Program Files/Git/bin")
-  (add-to-list 'exec-path "C:/Program Files/Git/mingw64/bin")
-  (setenv "PATH" (concat "C:/Program Files/Git/bin;" "C:/Program Files/Git/mingw64/bin;" (getenv "PATH")))
-  ;; needed for arc-mode
-  (add-to-list 'exec-path "C:/Program Files/7-Zip"))
+;;   (set-frame-font "Cascadia Code 14")
+;;   (add-to-list 'exec-path "C:/Program Files/Git/bin")
+;;   (add-to-list 'exec-path "C:/Program Files/Git/mingw64/bin")
+;;   (setenv "PATH" (concat "C:/Program Files/Git/bin;" "C:/Program Files/Git/mingw64/bin;" (getenv "PATH")))
+;;   ;; needed for arc-mode
+;;   (add-to-list 'exec-path "C:/Program Files/7-Zip"))
 
-;; config changes made through the customize UI will be stored here
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;; ;; config changes made through the customize UI will be stored here
+;; (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
-(when (file-exists-p custom-file)
-  (load custom-file))
+;; (when (file-exists-p custom-file)
+;;   (load custom-file))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(provide 'my-custom-short)
+;;(provide 'my-custom-short)
 ;;; custom.el ends here.
